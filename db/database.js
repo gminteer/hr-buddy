37353ba916622073efc88fc676b989d4f6ba9fd0
gminteer <gminteer@gmail.com>
2020-08-19 18:59:37 -0400

    added insert queries

--- db/database.js
+++ db/database.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 const sql = require('sql-template-strings');
 const mysql = require('mysql2/promise');
 // eslint-disable-next-line node/no-unpublished-require
@@ -30,14 +31,25 @@ const dbWrapper = {
     role: (id) => sql` WHERE role.id = ${id}`,
     employee: (id) => sql` WHERE emp.id = ${id}`,
   }),
+  ADD_SQL: Object.freeze({
+    department: ({name}) => sql`INSERT INTO department (name) VALUES (${name})`,
+    role: ({title, salary, department_id}) =>
+      sql`INSERT INTO role (title, salary, department_id) VALUES (${title}, ${salary}, ${department_id})`,
+    employee: ({first_name, last_name, role_id, manager_id}) => sql`INSERT INTO employee
+        (first_name, last_name, role_id, manager_id)
+        VALUES (${first_name}, ${last_name}, ${role_id}, ${manager_id})`,
+  }),
+  // error "handling"
   async _query(query) {
     try {
-      const [rows] = await this.connection.query(query);
+      // returns [rows, fields] 'fields' is some extra metadata this program hasn't found a use for yet...
+      const [rows] = await this.connection.execute(query);
       return rows;
     } catch (err) {
       return console.error(err);
     }
   },
+  // setup & teardown
   async init() {
     try {
       this.connection = await this.connection;
@@ -49,6 +61,7 @@ const dbWrapper = {
     // This doesn't need to be async, but then it'd be the only function on dbWrapper that isn't async, so I think it's simpler this way...
     return this.connection.end();
   },
+  // read
   async get(type, id) {
     if (!Object.keys(this.GET_SQL).includes(type)) return;
     if (id) return this.filter(type, this.GET_ONE_SQL[type](id));
@@ -71,6 +84,12 @@ const dbWrapper = {
     if (!id) return;
     const filter = sql` WHERE department_id = ${id}`;
     return this.filter('employee', filter);
+  },
+  // create
+  async add(type, answers) {
+    if (!Object.keys(this.ADD_SQL).includes(type)) return;
+    const query = this.ADD_SQL[type](answers);
+    return this._query(query);
   },
 };
 
