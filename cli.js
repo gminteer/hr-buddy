d59da448ea8e7a56969292c312aebc2a36b08fd7
gminteer <gminteer@gmail.com>
2020-08-23 23:30:15 -0400

    error when missing credentials

--- cli.js
+++ cli.js
@@ -1,9 +1,29 @@
 #!/usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const os = require('os');
+
 // eslint-disable-next-line no-unused-vars
 const consoleTable = require('console.table');
 const ui = require('./lib/ui');
-const dbWrapper = require('./lib/db-wrapper');
 (async () => {
+  try {
+    await fs.promises.access(path.join(__dirname, './db/connection.json'));
+  } catch (err) {
+    console.log('Missing MySQL connection info.');
+    console.log('You\'ll need to create a file called "connection.json" in the db/ folder with the following:');
+    console.log('{');
+    console.log('\t"host": "{{ MySQL HOSTNAME (probably "localhost") }}"');
+    console.log('\t"user": "{{ MySQL USERNAME (probably "root" or "dba") }}"');
+    console.log('\t"password": "{{ PASSWORD (probably "12345") }}"');
+    console.log(
+      '\t"database": "hr_buddy" (this can probably be anything as long as the user has create/drop table rights)'
+    );
+    console.log('}');
+    console.log('\n---\nExiting...');
+    process.exit(os.constants.errno.ENOENT);
+  }
+  const dbWrapper = require('./lib/db-wrapper');
   await dbWrapper.init();
   await ui(dbWrapper);
   await dbWrapper.end();
